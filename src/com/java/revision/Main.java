56e94c04c66089a895d4e8768838bff61eb2bd44
vishu221b <vishal.dogra738@gmail.com>
2020-08-03 03:56:05 +0530

    User Inputs

--- src/com/java/revision/Main.java
+++ src/com/java/revision/Main.java
@@ -3,15 +3,25 @@ package com.java.revision;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        UserInputs userInputs = new UserInputs();
         List<String> newStrings = new ArrayList<>();
         String[] arr = {"Veeshu", "Deeshu", "Java", "Shava"};
         Collections.addAll(newStrings, arr);
         System.out.println(newStrings.size());
         newStrings.forEach(
-                (e) -> System.out.println(e+" Printed :D"));
+                (e) -> System.out.println(e+" Printed :D")
+        );
+        Scanner inputScanner = userInputs.getScanner();
+        System.out.print("Gimme an integer input :D => ");
+        int someInteger = inputScanner.nextInt();
+        System.out.print("Now gimme a String input :D => ");
+        String someString = inputScanner.next();
+        System.out.println(String.format("User integer is %d and user string is %s.", someInteger, someString));
+
     }
 }
---
+++ src/com/java/revision/UserInputs.java
@@ -0,0 +1,12 @@
+package com.java.revision;
+
+import java.util.Scanner;
+
+public class UserInputs {
+    private Scanner sc = new Scanner(System.in);
+
+    public Scanner getScanner(){
+        return sc;
+    }
+
+}
